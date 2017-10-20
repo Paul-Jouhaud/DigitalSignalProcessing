53fba89ed87b26e4de191bd9d9d1e9049612bd9f
Paul Jouhaud <jouhaud.paul@gmail.com>
2017-10-20 14:36:11 +0200

    Update noise

--- main.m
+++ main.m
@@ -8,9 +8,8 @@ signal = signal + bruit(5, formant*formant', size(signal,2));
 recu = reception(signal, formant, SURECHANTILLONNAGE, TAILLE_FORMANT*SURECHANTILLONNAGE/2);
 recu = recu(1,1:size(message,2));
 sum(abs(recu-message)/2)
-%figure 1;
 %plot(recu);
-%saveas(recu,'signal_bruite.jpg');
+%saveas(plot(recu),'signal_bruite.jpg');
 %figure 2;
 %plot(recu - message);
 %saveas(plot(recu - message),'diff_noise.png')
