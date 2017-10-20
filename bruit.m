d28d3ed1f674aa8892b5d877af06d6ccd83cd213
Delcluze Mathieu <mathieu.delcluze@supelec.fr>
2017-10-20 14:30:24 +0200

    add

--- bruit.m
+++ bruit.m
@@ -1,1 +1,1 @@
-function bruit = bruit()end
+function bruit = bruit(rapport, energie_bit, taille_bruit)  bruit = randn(1, taille_bruit);  var = energie_bit / (2*10.^(rapport/10));  bruit = bruit * sqrt(var);end
--- main.m
+++ main.m
@@ -2,14 +2,15 @@ TAILLE_MESSAGE = 1000;
 SURECHANTILLONNAGE = 30;
 TAILLE_FORMANT = 100;
 message = 2 * (rand(1,TAILLE_MESSAGE) > 0.5) - 1;
-formant = formantcos(SURECHANTILLONNAGE*TAILLE_FORMANT+1, SURECHANTILLONNAGE); 
+formant = formantcos(SURECHANTILLONNAGE*TAILLE_FORMANT+1, SURECHANTILLONNAGE);
 signal = emission(message, formant, SURECHANTILLONNAGE);
-filtre = formant(1,end:-1:1) / (formant*formant');
-recu = reception(signal, filtre, SURECHANTILLONNAGE, 1+TAILLE_FORMANT*SURECHANTILLONNAGE/2); 
+signal = signal + bruit(5, formant*formant', size(signal,2));
+recu = reception(signal, formant, SURECHANTILLONNAGE, TAILLE_FORMANT*SURECHANTILLONNAGE/2);
 recu = recu(1,1:size(message,2));
 sum(abs(recu-message)/2)
-
-figure
-plot(recu)
-figure
-plot(recu-message)
+%figure 1;
+%plot(recu);
+%saveas(recu,'signal_bruite.jpg');
+%figure 2;
+%plot(recu - message);
+%saveas(plot(recu - message),'diff_noise.png')
