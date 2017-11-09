1a14773cd6c49f76cf3a95c788b7110db4b17fc9
Paul Jouhaud <jouhaud.paul@gmail.com>
2017-11-09 22:13:09 +0100

    Improve readability

--- courbe_canal_simulee_test1.m
+++ courbe_canal_simulee_test1.m
@@ -1,16 +1,15 @@
 teb_min_1 = zeros(1, 81);
 teb_max_1 = zeros(1, 81);
-x_canal = -2:0.01:4
+x_canal = -2:0.01:4;
 TAILLE_MESSAGE = 1000;
 SURECHANTILLONNAGE = 30;
 TAILLE_FORMANT = 100;
 formant_base = formantcos(SURECHANTILLONNAGE*TAILLE_FORMANT+1, SURECHANTILLONNAGE);
 formant_filtre = formantcos(SURECHANTILLONNAGE*TAILLE_FORMANT+1, SURECHANTILLONNAGE);
 canal = 1;
-%canal = heaviside(x_canal).*exp(-x_canal);
+
 for x = (0:0.1:8)
   i = int8(x*10 + 1);
-  disp(i);
   current_teb_min = 1;
   current_teb_max = 0;
   for y=1:10
@@ -26,9 +25,5 @@ for x = (0:0.1:8)
   teb_max_1(i) = current_teb_max;
 endfor
 figure 1;
-plot((0:0.1:8), teb_min_1, (0:0.1:8), teb_max_1)
-%saveas(plot((0:0.1:8), teb_min, (0:0.1:8), teb_max), 'erreur_binaire_simulee2.png')
-
-%x = (0:0.1:8);
-%y = 0.5 * erfc(sqrt(10.^(x/10)));
-%saveas(plot((0:0.1:8), teb_min, (0:0.1:8), teb_max, (0:0.1:8), y), 'diff_erreur_binaire_simulee.png')
+plot((0:0.1:8), teb_min_1, (0:0.1:8), teb_max_1);
+saveas(plot((0:0.1:8), teb_min_1, (0:0.1:8), teb_max_1), 'courbe_canal_simulee_test1.png')
