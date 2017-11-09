75a5a3315262c240e4353d158f3054e3fd8467c1
Paul Jouhaud <jouhaud.paul@gmail.com>
2017-11-09 22:12:15 +0100

    Fix indent

--- erreur_canal.m
+++ erreur_canal.m
@@ -1,7 +1,7 @@
 function erreur = erreur_canal(rapport, taille_message, SURECHANTILLONNAGE, formant_base, formant_filtre, canal)
     message = 2 * (rand(1,taille_message) > 0.5) - 1;
     signal = emission(message, formant_base, SURECHANTILLONNAGE);
-        signal = conv(signal, canal);
+    signal = conv(signal, canal);
     signal = signal + bruit(rapport, signal*signal'/taille_message, size(signal,2));
     recu = reception(signal, formant_filtre, SURECHANTILLONNAGE, ...
     1 + (length(formant_base) + length(canal) - 2) / 2); 
